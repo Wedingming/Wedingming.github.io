0282835f00705508514f26e8a5392547ad0193c1
未定名 <179002797+Wedingming@users.noreply.github.com>
2025-02-22 17:27:17 +0800

    更新 contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,12 @@
-contact
+<script>
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var message = document.getElementById("message").value;
+
+    if (name === "" || message === "") {
+        alert("名字和留言内容不能为空！");
+        return false; // 阻止表单提交
+    }
+    return true; // 允许表单提交
+}
+</script>
